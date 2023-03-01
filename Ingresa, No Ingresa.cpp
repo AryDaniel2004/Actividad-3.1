3bad71fc38041fb5bdfe4d7b4a08f6517094b12a
AryDaniel2004 <126044674+AryDaniel2004@users.noreply.github.com>
2023-02-28 19:18:52 -0600

    Add files via upload

---
+++ Ingresa, No Ingresa.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+main(){
+char lapiz,lapicero;
+cout<<"Trae Lapiz (s/n):";
+cin>>lapiz;
+cout<<"Trae lapicero (s/n):";
+cin>>lapicero;
+
+	if (lapiz=='s' || lapicero=='s' ){
+			cout<<lapiz<<endl;
+		cout<<"Ingresa"<<endl;
+	}else{
+			cout<<"Lo siento no puede ingresar"<<endl;
+		}
+	
+ 	
+system("pause");	
+}
---
+++ Pais.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main(){
+string pais="";	
+cout<<"Ingrese Pais:";
+cin>>pais;
+
+	if (pais=="guatemala"){
+		cout<<"yo soy de"<<pais<<"mi pais"<<endl;
+	}else{
+		cout<<pais<<endl;
+	}
+ 	
+system("pause");	
+}
